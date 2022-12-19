6a3dd9f27354b0957f593a06fd7790eaf2e44f9d
cphys <22038435+cphys@users.noreply.github.com>
2022-12-19 06:50:12 -0500

    HAAAANDSgit add *git add *

--- weyl.m
+++ weyl.m
@@ -1,47 +1,95 @@
 ClearAll["Global`*"]
 
+(* define the pauli matricies for particle hole and spin spaces *)
 tau[dim_, numb_] := 
- KroneckerProduct[PauliMatrix[numb], IdentityMatrix[dim]]
-
+ tau[dim, numb] = 
+  KroneckerProduct[PauliMatrix[numb], IdentityMatrix[dim]]
 sigma[dim_, numb_] := 
- KroneckerProduct[IdentityMatrix[dim], PauliMatrix[numb]]
+ sigma[dim, numb] = 
+  KroneckerProduct[IdentityMatrix[dim], PauliMatrix[numb]]
+  
+(* The Hamiltonian for the system *)
+H4Lat[kx_, ky_, kz_, t_, tz_, b_, \[Lambda]_, 
+  Bz_] := (t*(Cos[kx] + Cos[ky] - 2) + tz*(Cos[kz] - b))*
+   sigma[2, 0] . tau[2, 3] + \[Lambda]*Sin[kx]*
+   sigma[2, 1] . tau[2, 1] + \[Lambda]*Sin[ky]*
+   sigma[2, 2] . tau[2, 1] + Bz*(sigma[2, 3] . tau[2, 0])
+   
+(* Find the Eigenvalues of the system *)
+eigs[kx_, ky_, kz_, i_, Bz_] := 
+ eigs[kx, ky, kz, i, Bz] = 
+  N[Eigenvalues[H4Lat[kx, ky, kz, 1., 1., 1., 1., Bz]]][[i]]
+  
+tabEig[i_, Bz_] := 
+ tabEig[i, Bz] = 
+  N[ParallelTable[
+    eigs[kx, ky, kz, i, Bz], {kx, -3.14, 3.14, .05}, {ky, -3.14, 
+     3.14, .05}, {kz, -3.14, 3.14, .05}]]
+     
+totalE[Bz_] := Sum[(-1)^i*Abs[tabEig[i, Bz]], {i, 1, 4}]
 
-k = {0, ky, kz};
-
-Hdeg = vf*Sum[k[[i]]*sigma[2, i], {i, 1, 3}] . tau[2, 3];
-
-HD[vf_, g_, Bz_] := 
-  vf*Sum[k[[i]]*sigma[2, i], {i, 1, 3}] . tau[2, 3] + 
-   g*Bz*(sigma[2, 3] . tau[2, 0]);
-
-Eigs[Bz_, g_, vf_, ky_, kz_] :=
- Eigs[Bz, g, vf, ky, kz] = {
-   -Sqrt[Bz^2 g^2 - 2 Bz g kz vf + (ky^2 + kz^2) vf^2],
-   Sqrt[Bz^2 g^2 - 2 Bz g kz vf + (ky^2 + kz^2) vf^2],
-   -Sqrt[Bz^2 g^2 + 2 Bz g kz vf + (ky^2 + kz^2) vf^2],
-   Sqrt[Bz^2 g^2 + 2 Bz g kz vf + (ky^2 + kz^2) vf^2]}
-
-tabEig[Bz_, i_] := 
-  tabEig[Bz, i] = 
-   N[ParallelTable[
-     N[Eigs[Bz, 1., 1., ky, kz][[i]]], {ky, -3.14, 
-      3.14, .1}, {kz, -3.14, 3.14, .1}]];
-
-graphicsTable = Table[
-   ListPlot3D[
-    {tabEig[Bz, 1], tabEig[Bz, 2], tabEig[Bz, 3], tabEig[Bz, 4]},
-    BoxRatios -> {1, 1, 1},
-    PlotTheme -> "Detailed",
-    DataRange -> {{-\[Pi], \[Pi]}, {-\[Pi], \[Pi]}},
-    ViewPoint -> {Pi/2, -Pi, .95},
-    AxesLabel -> {"\!\(\*SubscriptBox[\(k\), \(y\)]\)", 
-      "\!\(\*SubscriptBox[\(k\), \(z\)]\)", "E"},
-    Ticks -> {{-\[Pi], 0, \[Pi]}, {\[Pi], 0, \[Pi]}, {-5, -3, -1, 0, 
-       1, 3, 5}},
-    PlotRange -> {-5, 5}],
-   {Bz, 0, 3, .01}];
-
+densPlot[numbs_, Bz_] :=
+ ListDensityPlot3D[
+  (-1)^numbs*Abs[tabEig[numbs, Bz]],
+  ImageSize -> 400,
+  ImagePadding -> {{50, 50}, {30, 10}},
+  BoxRatios -> {1, 1, 1},
+  PlotTheme -> "Detailed",
+  AxesLabel -> {Style["\!\(\*SubscriptBox[\(k\), \(x\)]\)", 
+     FontWeight -> Bold, FontSize -> 24],
+    Style["\!\(\*SubscriptBox[\(k\), \(y\)]\)", FontWeight -> Bold, 
+     FontSize -> 24],
+    Style["\!\(\*SubscriptBox[\(k\), \(z\)]\)", FontWeight -> Bold, 
+     FontSize -> 24]},
+  Ticks -> {{-\[Pi], 0, \[Pi]}, {-\[Pi], 0, \[Pi]}, {-\[Pi], 
+     0, \[Pi]}},
+  TicksStyle -> Directive[14],
+  PlotLegends -> Placed[BarLegend[{"SunsetColors", {0, 3}}], Below],
+  DataRange -> {{-3.2, 3.2}, {-3.2, 3.2}, {-3.2, 3.2}},
+  OpacityFunction -> Interval[{-.5, .5}],
+  OpacityFunctionScaling -> False ,
+  ColorFunction -> "SunsetColors"]
+  
+contPlot[numbs_, Bz_] :=
+ ListDensityPlot3D[
+  (-1)^numbs*Abs[tabEig[numbs, Bz]],
+  ImageSize -> 400,
+  ImagePadding -> {{50, 50}, {30, 10}},
+  PlotTheme -> "Detailed",
+  AxesLabel -> {Style["\!\(\*SubscriptBox[\(k\), \(x\)]\)", 
+     FontWeight -> Bold, FontSize -> 24],
+    Style["\!\(\*SubscriptBox[\(k\), \(y\)]\)", FontWeight -> Bold, 
+     FontSize -> 24],
+    Style["\!\(\*SubscriptBox[\(k\), \(z\)]\)", FontWeight -> Bold, 
+     FontSize -> 24]},
+  Ticks -> {{-\[Pi], 0, \[Pi]}, {-\[Pi], 0, \[Pi]}, {-\[Pi], 
+     0, \[Pi]}},
+  TicksStyle -> Directive[14],
+  PlotLegends -> Placed[BarLegend[{"SunsetColors", {0, 7}}], Below],
+  DataRange -> {{-3.2, 3.2}, {-3.2, 3.2}, {-3.2, 3.2}},
+  PlotRange -> {{-3.2, 3.2}, {0, 3.2}, {-3.2, 3.2}} ,
+  BoxRatios -> Automatic,
+  ColorFunction -> "SunsetColors",
+  OpacityFunction -> None,
+  OpacityFunctionScaling -> False]
+  
+densPlotTotalE[Bz_] :=
+ ListDensityPlot3D[
+  totalE[Bz],
+  BoxRatios -> {1, 1, 1},
+  PlotTheme -> "Detailed",
+  ViewPoint -> {Pi/2, -Pi, .65},
+  AxesLabel -> {"k_x", "\!\(\*SubscriptBox[\(k\), \(y\)]\)", 
+    "\!\(\*SubscriptBox[\(k\), \(z\)]\)"},
+  Ticks -> {{-\[Pi], 0, \[Pi]}, {\[Pi], 0, \[Pi]}, {\[Pi], 
+     0, \[Pi]}},
+  DataRange -> {{-3.2, 3.2}, {-3.2, 3.2}, {-3.2, 3.2}}]
+  
+graphicsTable = 
+  Table[Row[{contPlot[2, B], contPlot[4, B], densPlot[2, B], 
+     densPlot[4, B]}], {B, 0, 2, .25}];
+     
 Export[
- FileNameJoin[{NotebookDirectory[], "testWeylgif2.gif"}],
- graphicsTable,
- "DisplayDurations" -> 0.04]
+  FileNameJoin[{NotebookDirectory[], "weylDensity.gif"}],
+  graphicsTable,
+  "DisplayDurations" -> 0.4];
